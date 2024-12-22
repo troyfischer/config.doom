61d51e65472330d157d35c292a8b4a1aede99370
troyfischer <troytfischer@gmail.com>
2024-12-22 15:08:46 -0600

    commit current doom config

---
+++ custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(package-selected-packages '(clang-format))
+ '(safe-local-variable-values '((lsp-clangd-compile-commands-dir . "build")))
+ '(warning-suppress-types '((org-element-cache) (defvaralias))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'projectile-ripgrep 'disabled nil)
